7a3e843788a1e7cb110403a57a41fa5bbe6450ba
KostuX <goldiskiker@gmail.com>
2022-10-23 22:26:02 +0100

    d

--- pages/api/db_Helper.js
+++ pages/api/db_Helper.js
@@ -6,4 +6,5 @@ export const connection = mysql.createConnection({
     password: '9ccac70f',
     port: 3306,
     database: 'heroku_54b7530745c0825'
-  });//
+  });
+  //mysql://b2c2ebcba0073b:9ccac70f@eu-cdbr-west-03.cleardb.net/heroku_54b7530745c0825?reconnect=true
