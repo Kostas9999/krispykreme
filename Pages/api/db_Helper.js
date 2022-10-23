da80b78fd5f18de76f622023e3392cfb292535ba
Kostas9999 <91334044+Kostas9999@users.noreply.github.com>
2022-10-23 17:41:18 +0100

    Delete Pages directory

--- Pages/api/db_Helper.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-
-export const connection = mysql.createConnection({
-    host: 'eu-cdbr-west-03.cleardb.net',
-    user: 'b2c2ebcba0073b',
-    password: '9ccac70f',
-    port: 3306,
-    database: 'heroku_54b7530745c0825'
-  });//
