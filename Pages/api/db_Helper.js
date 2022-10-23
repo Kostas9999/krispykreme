bd7d37305cb8baae19ad077833ef242e7440cdfa
KostuX <goldiskiker@gmail.com>
2022-10-23 17:22:05 +0100

    db deploy

--- Pages/api/db_Helper.js
+++ Pages/api/db_Helper.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 
 export const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
+    host: 'eu-cdbr-west-03.cleardb.net',
+    user: 'b2c2ebcba0073b',
+    password: '9ccac70f',
     port: 3306,
-    database: 'assignment1'
+    database: 'heroku_54b7530745c0825'
   });
