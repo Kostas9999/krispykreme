1c4c46f081745d0dde81584747a38af871e9f3bc
KostuX <goldiskiker@gmail.com>
2022-10-23 17:32:05 +0100

    Update db.js

--- Pages/api/db.js
+++ Pages/api/db.js
@@ -6,6 +6,12 @@ export default function handler(req, res) {
 
      const username = req.body.username;
      const pass = req.body.password;
+
+
+     connection.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+    });
    
 
   connection.query(
