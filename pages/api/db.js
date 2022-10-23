15dbc2b1917192a2a1dfd5bf48a0b0577fe32d3a
KostuX <goldiskiker@gmail.com>
2022-10-24 00:09:54 +0100

    Update db.js

--- pages/api/db.js
+++ pages/api/db.js
@@ -3,26 +3,25 @@ require('./db_Helper');
 import { connection } from './db_Helper'
 
 export default function handler(req, res) {
-
+  
      const username = req.body.username;
      const pass = req.body.password;
 
-
      connection.connect(function(err) {
       if (err) throw err;
-      console.log("Connected!");
+      console.log("Connected to db!");
     });
    
 
   connection.query(
     "SELECT * FROM users WHERE username = '"+username+"' AND pass = '"+pass+"' LIMIT 1;",
-    function(err, results, fields) {
-      console.log("ok");
+ //   function(err, results, fields) {
+     
       // sending back the result.
      // if(results.length == 1){res.status(200).json("ok");} 
     //  else { res.status(200).json("fail");}
      
-    }
+  //  }
     
   );
 
