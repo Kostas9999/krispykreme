ee226612c3991f98cbd47b39b3a6b7393787160e
Kostas9999 <b00148740@mytudublin.ie>
2022-10-24 12:27:16 +0100

    Update db.js

--- pages/api/db.js
+++ pages/api/db.js
@@ -12,20 +12,18 @@ export default function Handler(req, res) {
   connection.query(
     "SELECT * FROM users WHERE username = '"+username+"' AND pass = '"+pass+"' LIMIT 1;",
     function(err, results, fields) {
-     
-      if(err){throw err;}
 
-      if(results.length == 1){res.status(200).json("ok");} 
-     else { res.status(200).json("fail");}
-
-      // sending back the result.
-     if(results.length == 1){res.status(200).json("ok");} 
-     else { res.status(200).json("fail");}
+ 
      
     }
     
-  ).then((res)=> {
-    console.log(res);
+  ).then((results)=> {
+    if(results.length == 1){res.status(200).json("ok");} 
+    else { res.status(200).json("fail");}
+
+     // sending back the result.
+    if(results.length == 1){res.status(200).json("ok");} 
+    else { res.status(200).json("fail");}
   
   });
  
