1e36ff025e48b52f7ae860e967eb05ccba5cca00
KostuX <goldiskiker@gmail.com>
2022-11-16 20:11:33 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Navbar from '../templates/navbar';
-import { NextUIProvider } from "@nextui-org/react"
+import { Button, NextUIProvider } from "@nextui-org/react"
 import { Container, Card, Row, Text, Col, Spacer , Grid} from "@nextui-org/react";
 import React from "react";
 
@@ -13,50 +13,107 @@ export default function Home({data}) {
 
  const list = [
   {
-    title: "Orange",
+    title: "Donut",
     img: "images/donuts-1.png",
-    price: "$5.50",
+    price: "5.50eur",
   },
   {
-    title: "Tangerine",
+    title: "Donut",
     img: "images/donuts-2.png",
-    price: "$3.00",
+    price: "3.00eur",
   },
   {
-    title: "Cherry",
+    title: "Donut",
     img: "images/donuts-3.png",
-    price: "$10.00",
+    price: "3.00eur",
   },
   {
-    title: "Lemon",
+    title: "Donut",
     img: "images/donuts-4.png",
-    price: "$5.30",
+    price: "3.00eur",
   },
   {
-    title: "Avocado",
+    title: "Donut",
     img: "images/donuts-5.png",
-    price: "$15.70",
+    price: "3.00eur",
   },
   {
-    title: "Lemon 2",
+    title: "Donut 2",
     img: "images/donuts-6.png",
-    price: "$8.00",
+    price: "3.00eur",
   },
   {
-    title: "Banana",
+    title: "Donut",
     img: "images/donuts-7.png",
-    price: "$7.50",
+    price: "3.00eur",
   },
   {
-    title: "Watermelon",
+    title: "Donut",
     img: "images/donuts-8.png",
-    price: "$12.20",
+    price: "3.00eur",
+  },
+  {
+    title: "Donut",
+    img: "images/donuts-8.png",
+    price: "3.00eur",
+  },
+  {
+    title: "Donut",
+    img: "images/donuts-7.png",
+    price: "3.00eur",
+  },
+  {
+    title: "Donut",
+    img: "images/donuts-8.png",
+    price: "3.00eur",
+  },
+  {
+    title: "Donut",
+    img: "images/donuts-8.png",
+    price: "3.00eur",
   },
 ];
   
   return (
 <NextUIProvider>
 <Navbar />
+
+<Container>
+      <Card css={{ $$cardColor: '$colors$primary' }}>
+      <Card.Body css={{ p: 0 }}>
+              <Card.Image
+                src={"https://filedn.eu/laylI9rT8UjYMnCgviybMrh/KrispyKreme/images/donuts-3.png" }
+                objectFit="cover"
+                width="100%"
+                height={200}
+                
+              />
+            </Card.Body>
+            <Card.Footer
+      isBlurred
+      css={{
+        position: "absolute",
+        bgBlur: "#ffffff66",
+        borderTop: "$borderWeights$light solid $gray800",
+        bottom: 0,
+        zIndex: 1,
+      }}
+    >
+
+<Text
+                
+                size={20}
+                weight="bold"
+                transform="uppercase"
+              >
+               Todays Specials
+              </Text>
+
+      </Card.Footer>
+      </Card>
+    </Container>
+  
+
 
 
     <Grid.Container gap={2} justify="flex-start">
@@ -68,23 +125,56 @@ export default function Home({data}) {
                 src={"https://filedn.eu/laylI9rT8UjYMnCgviybMrh/KrispyKreme/" + item.img}
                 objectFit="cover"
                 width="100%"
-                height={140}
+                height={240}
                 alt={item.title}
               />
+
+                <Text
+                color="blue"
+                size={12}
+                weight="bold"
+                transform="uppercase"
+              >
+                Add to Cart 
+              </Text>
             </Card.Body>
-            <Card.Footer css={{ justifyItems: "flex-start" }}>
+            <Card.Footer
+
+            
+      isBlurred
+      css={{
+        position: "absolute",
+        bgBlur: "#ffffff66",
+        borderTop: "$borderWeights$light solid $gray800",
+        bottom: 0,
+        zIndex: 1,
+      }}
+    >
               <Row wrap="wrap" justify="space-between" align="center">
-                <Text b>{item.title}</Text>
-                <Text css={{ color: "$accents7", fontWeight: "$semibold", fontSize: "$sm" }}>
-                  {item.price}
-                </Text>
+              {item.price}
+
+              <Button
+              flat
+              auto
+              rounded
+              css={{ color: "primary", bg: "#94f9f026" }}
+            >
+              <Text
+                color="blue"
+                size={12}
+                weight="bold"
+                transform="uppercase"
+              >
+                Add to Cart 
+              </Text>
+            </Button>
               </Row>
             </Card.Footer>
           </Card>
         </Grid>
       ))}
     </Grid.Container>
-  );
+ 
 
 </NextUIProvider>
 
