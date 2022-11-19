5a25a4cf1a8b6fad8e313837a9706aa660594ba7
KostuX <goldiskiker@gmail.com>
2022-11-19 14:13:33 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -118,13 +118,13 @@ export default function Home({productss}) {
   
 }
 
-export async function getServerSideProps() {
+//export async function getServerSideProps() {
   // Fetch data from external API
  // const res = await fetch('http://localhost:3000/api/db_getProducts')
  // const productss = await res.json()
 
-  products = [{"productID":1,"title":"Donut","price":0.1,"img":"donuts-1.png","description":"desc"},]
+//  products = [{"productID":1,"title":"Donut","price":0.1,"img":"donuts-1.png","description":"desc"},]
 
   // Pass data to the page via props
-  return { props: { productss } }
-}
+//  return { props: { productss } }
+//}
