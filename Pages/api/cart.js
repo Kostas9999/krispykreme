55f927620f09bfc322fb6067f9b04b7a3ea33833
KostuX <goldiskiker@gmail.com>
2022-11-20 18:19:28 +0000

    d

--- Pages/api/cart.js
+++ Pages/api/cart.js
@@ -29,7 +29,7 @@ export default withIronSessionApiRoute(
       password: "complex_password_at_least_32_characters_long",
       // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
       cookieOptions: {
-        secure: false //process.env.NODE_ENV === "production",
+        secure: true //process.env.NODE_ENV === "production",
       },
     },
   );
--- pages/checkout.js
+++ pages/checkout.js
@@ -73,7 +73,7 @@ cart.forEach((product, i) =>
     password: "complex_password_at_least_32_characters_long",
     // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
     cookieOptions: {
-      secure: false //process.env.NODE_ENV === "production",
+      secure: true //process.env.NODE_ENV === "production",
     },
   },
 );
